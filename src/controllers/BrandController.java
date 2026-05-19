d1cebf2b370fabfcc43cfb4f2f7d25b06ee3e76a
Your Name <you@example.com>
2026-05-19 23:59:38 +0000

    Acabar en la casa

--- src/controllers/BrandController.java
+++ src/controllers/BrandController.java
@@ -1,5 +1,23 @@
 package controllers;
 
-public class BrandController {
+import models.Brand;
 
+public class BrandController {
+  public Brand[] sortBubbleDesc(Brand[] brands) {
+
+    for (int i = 0; i < brands.length - 1; i++) {
+
+      for (int j = 0; j < brands.length - 1 - i; j++) {
+
+        if (brands[j].getTotalValidYears() < brands[j + 1].getTotalValidYears()) {
+
+          Brand aux = brands[j];
+          brands[j] = brands[j + 1];
+          brands[j + 1] = aux;
+        }
+      }
+    }
+
+    return brands;
+  }
 }
--- src/models/Brand.java
+++ src/models/Brand.java
@@ -1,5 +1,57 @@
 package models;
 
+import java.time.Year;
+import java.util.Arrays;
+
+import org.junit.platform.console.shadow.picocli.CommandLine.Model;
+
 public class Brand {
+  private String brandName;
+  private CarModel[] models;
+
+  public Brand(String brandName, CarModel[] models) {
+    this.brandName = brandName;
+    this.models = models;
+  }
+
+  public String getBrandName() {
+    return brandName;
+  }
+
+  public void setBrandName(String brandName) {
+    this.brandName = brandName;
+  }
+
+  public CarModel[] getModels() {
+    return models;
+  }
+
+  public void setModels(CarModel[] models) {
+    this.models = models;
+  }
+
+  public int getTotalValidYears() {
+
+    int total = 0;
+
+    for (int i = 0; i < models.length; i++) {
+
+      CarYear[] years = models[i].getYears();
+
+      for (int j = 0; j < years.length; j++) {
+
+        if (years[j].isValid()) {
+          total++;
+        }
+      }
+    }
+
+    return total;
+  }
+
+  @Override
+  public String toString() {
+    return "Brand [brandName=" + brandName + ", models=" + Arrays.toString(models) + "]";
+  }
 
 }
